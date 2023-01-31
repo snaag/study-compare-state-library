00b06c51761d8f21de70d0bd9d4a230f06595ea1
snaag <snaag.dev@gmail.com>
2023-01-31 23:40:37 +0900

    TodoItem 에 constructor 추가

--- src/domain/TodoItem.js
+++ src/domain/TodoItem.js
@@ -1,5 +1,11 @@
-class TodoItem {
+export class TodoItem {
     id;
     text;
     isDone;
+
+    constructor(id, text, isDone=false) {
+        this.id = id;
+        this.text = text;
+        this.isDone = isDone;
+    }
 }
