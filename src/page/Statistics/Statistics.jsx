0fbc2073d82e06777140ce038e4bea71b47703d4
snaag <snaag.dev@gmail.com>
2023-02-01 15:15:10 +0900

    add style

--- src/page/Statistics/Statistics.jsx
+++ src/page/Statistics/Statistics.jsx
@@ -4,6 +4,12 @@ const Statistics = () => {
     return (
         <div>
             <h2>Statistics</h2>
+            <div className="graph">
+                <div className="block">
+                {/*    완료된 경우에는 block done */}
+                {/*    진행중인 경우에는 block */}
+                </div>
+            </div>
         </div>
     );
 };
